464657d3ebcb81356a8a178601bf069aa93e039e
ayocodingit <ayocodingit@gmail.com>
2022-10-07 12:16:25 +0700

    add replace participant

--- src/providers/telegram/user.service.ts
+++ src/providers/telegram/user.service.ts
@@ -42,7 +42,7 @@ export class UserService {
     for (const participant of participants) {
       const isFound = false
       const telegramUser = this.searchTelegramUsers(telegramUsers, isFound, participant)
-      if (!telegramUser.isFound) users.push(participant)
+      if (!telegramUser.isFound) users.push(participant.replace(' ', ''))
       else users.push(telegramUser.result)
     }
     return users
