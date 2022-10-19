5e46c548c7ba73380849af15fce059498fdedd72
ayocodingit <ayocodingit@gmail.com>
2022-10-19 23:05:01 +0700

    add random delay queue options

--- src/providers/queue/queue.service.ts
+++ src/providers/queue/queue.service.ts
@@ -20,19 +20,25 @@ export class QueueService {
     lifo: true,
   }
 
+  randomDelayQueueOptions = () => {
+    const jobOptions = this.queueOptions
+    jobOptions.delay = this.queueOptions.delay * Math.floor(Math.random() * 30)
+    return jobOptions
+  }
+
   eventMergeGithub = (data: any) => {
-    this.github.add('event-merge', data, this.queueOptions)
+    this.github.add('event-merge', data, this.randomDelayQueueOptions())
   }
 
   eventMergeGitlab = (data: any) => {
-    this.gitlab.add('event-merge', data, this.queueOptions)
+    this.gitlab.add('event-merge', data, this.randomDelayQueueOptions())
   }
 
   eventClickup = (event: string, data: any) => {
-    this.clickup.add(event, data, this.queueOptions)
+    this.clickup.add(event, data, this.randomDelayQueueOptions())
   }
 
   eventQase = (data: any) => {
-    this.qase.add('event-qase', data, this.queueOptions)
+    this.qase.add('event-qase', data, this.randomDelayQueueOptions())
   }
 }
