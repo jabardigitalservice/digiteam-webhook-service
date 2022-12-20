b29a8fde01c51a8c40518ea1188c17b510e2826d
ayocodingit <ayocodingit@gmail.com>
2022-12-20 16:16:27 +0700

    config: add logger with winston

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,24 @@ import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import helmet from 'helmet'
 import { AppModule } from './app.module'
+import logger from './common/helpers/logger'
 import { loadSwagger } from './common/swagger/swagger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  })
 
   const configService = app.get(ConfigService)
   app.enableCors()
   app.use(helmet())
+  app.useLogger(logger)
 
   loadSwagger(configService.get('app.name'), app)
 
-  await app.listen(configService.get('app.port'))
+  const PORT: number = configService.get('app.port')
+  await app.listen(PORT).then(() => {
+    logger.log(`🚀 Server ready at http://0.0.0.0:${PORT}`)
+  })
 }
 bootstrap()
--- src/providers/elastic/elastic.service.ts
+++ src/providers/elastic/elastic.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { ElasticsearchService } from '@nestjs/elasticsearch'
 import moment from 'moment'
+import logger from 'src/common/helpers/logger'
 import { Evidence } from 'src/interface/evidence.interface'
 
 @Injectable()
 export class ElasticService {
   constructor(
     private elasticSearchService: ElasticsearchService,
-    private configService: ConfigService
+    private configService: ConfigService,
   ) {}
 
   getIndex = () => {
@@ -16,6 +17,12 @@ export class ElasticService {
   }
 
   create = async (data: any) => {
+    logger.log(
+      JSON.stringify({
+        createdAt: moment().toISOString(),
+        ...data,
+      })
+    )
     return this.elasticSearchService.index({
       index: this.getIndex(),
       body: {
@@ -29,6 +36,13 @@ export class ElasticService {
     const { participants } = evidence
     for (const participant of participants) {
       if (!participant) continue
+      logger.log(
+        JSON.stringify({
+          project: evidence.project,
+          participant,
+          ...evidence,
+        })
+      )
       this.create({
         project: evidence.project,
         participant,
